db6412237c8c2aef45ec3fc0498e7e24b7083664
khrstudio <khr.studio@gmail.com>
2017-12-27 21:16:58 +0600

    new file

---
+++ Day_16/syntex.php
@@ -0,0 +1,56 @@
+<?php
+
+//   //echo 'Hello World';
+//    echo '<html>';
+//        echo '<head>';
+//            echo '<title>';
+//                echo 'HOME';
+//            echo '</title>';
+//        echo '</head>';
+//        echo '<body>';
+//            echo '<h1>This is a Heading</h1>';
+//            echo '<p>This is a Paragraph</p>';
+//        echo '</body>';
+//    echo "</html>";
+?>
+
+<!--<html>-->
+<!--    <head>-->
+<!--        <title>--><?php //echo 'HOME'; ?><!--</title>-->
+<!--    </head>-->
+<!--    <body>-->
+<!--        <h1>--><?php //echo 'This is a Heading';?><!--</h1>-->
+<!--        <p>--><?php //echo 'This is a Heading';?><!--</p>-->
+<!--    </body>-->
+<!--</html>-->
+
+<?php
+//$data = [];
+//
+//$data[0] = 10;
+//$data[1] = 20;
+//$data[2] = 30;
+//
+//$data['name'] = 'Sajib Khan';
+//$data['city'] = 'Dhaka';
+//$data['country'] = 'Bangladesh';
+//
+////foreach ($data as $value){
+////    echo $value.' ';
+////}
+//echo '<pre>';
+////print_r($data);
+//var_dump($data)
+$data = [
+    'name' => 'Sajib Khan',
+    'city' => 'Dhaka',
+    'country' => 'Bangladesh',
+    'mobile' => '01718182221'
+];
+
+echo '<pre>';
+print_r($data);
+
+
+
+?>
